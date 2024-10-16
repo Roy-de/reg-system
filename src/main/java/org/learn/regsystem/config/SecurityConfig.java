117d15ab193b7d54f88904055bd504247f51f29d
Roy-de <roymuri28@example.com>
2024-10-16 13:09:50 +0300

    Disabled security form login

--- src/main/java/org/learn/regsystem/config/SecurityConfig.java
+++ src/main/java/org/learn/regsystem/config/SecurityConfig.java
@@ -6,34 +6,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.cors.CorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.addAllowedOrigin("*");
-        corsConfiguration.addAllowedHeader("*");
-        corsConfiguration.addAllowedMethod("*");
-        corsConfiguration.addExposedHeader("Authorization");
-        corsConfiguration.addExposedHeader("Access-Control-Allow-Origin");
-        corsConfiguration.addExposedHeader("Access-Control-Allow-Methods");
-        corsConfiguration.addExposedHeader("Access-Control-Max-Age");
-        corsConfiguration.addExposedHeader("Access-Control-Allow-Headers");
-        corsConfiguration.addExposedHeader("Access-Control-Allow-Credentials");
-        UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
-        corsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-        return corsConfigurationSource;
-    }
-    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.cors(c -> c.configurationSource(corsConfigurationSource()))
+        return http.cors(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(r -> r
                         .anyRequest().permitAll())
                 .formLogin(AbstractHttpConfigurer::disable)
